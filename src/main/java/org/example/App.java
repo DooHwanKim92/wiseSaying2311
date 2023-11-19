45d995ba53d6eb73ddbc48015848e9dc886277dd
DooHwanKim92 <rmfpsvldj@gmail.com>
2023-11-19 17:10:50 +0900

    step-2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,13 +9,21 @@ public class App {
     }
     void run() {
         System.out.println("== 명언 앱 ==");
-
+        int i = 0;
         while (true) {
             System.out.print("명령) ");
-            String command = this.sc.nextLine();
+            String command = this.sc.nextLine().trim();
 
             if(command.equals("종료")) {
                 break;
+            }
+            else if(command.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = this.sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String author = this.sc.nextLine().trim();
+                i++;
+                System.out.println(i+"번 명언이 등록되었습니다.");
             }
         }
     }
